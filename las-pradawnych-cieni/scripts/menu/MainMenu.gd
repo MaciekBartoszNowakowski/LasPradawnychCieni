a1b049bd22df929943914f173ee0003310825a25
unknown <bieniaszkacper03@gmail.com>
2026-05-01 20:02:16 +0200

    add rest room

--- las-pradawnych-cieni/scripts/menu/MainMenu.gd
+++ las-pradawnych-cieni/scripts/menu/MainMenu.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var btn_exit: Button = $UIRoot/CenterBox/MenuColumn/BoardPanel/BoardVBox/BtnExit
 
 @onready var audio_music: AudioStreamPlayer = $AudioMusic
-@onready var audio_ui: AudioStreamPlayer = $AudioUI
 
 const MAP_SCENE_PATH := "res://scenes/map/Map.tscn"
 
@@ -13,17 +12,14 @@ func _ready() -> void:
 	btn_new_game.pressed.connect(_on_btn_new_game_pressed)
 	btn_exit.pressed.connect(_on_btn_exit_pressed)
 
-func _play_click() -> void:
-	audio_ui.play()
-
 func _on_btn_new_game_pressed() -> void:
-	_play_click()
+	UiAudio.play_click()
 	await get_tree().create_timer(0.08).timeout
 	MapState.reset_map()
 	GameState.player_team = Team.new()
 	get_tree().change_scene_to_file(MAP_SCENE_PATH)
 
 func _on_btn_exit_pressed() -> void:
-	_play_click()
+	UiAudio.play_click()
 	await get_tree().create_timer(0.08).timeout
 	get_tree().quit()
